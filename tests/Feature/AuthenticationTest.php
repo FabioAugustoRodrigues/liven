cfa0c5d1fa3d55a06a6f50e6258cb6880d18692c
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2024-06-20 17:09:36 -0300

    test: authentication tests

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_successful_user_login()
+    {
+        $user = User::factory()->create([
+            'name' => 'Example name',
+            'email' => 'email@example.com',
+            'password' => 'examplepassword123'
+        ]);
+
+        $credentials = [
+            'email' => 'email@example.com',
+            'password' => 'examplepassword123',
+        ];
+
+        $response = $this->postJson('/api/users/login', $credentials);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'success',
+                'data' => [
+                    'access_token',
+                    'token_type',
+                    'expires_in',
+                ],
+                'message',
+            ])
+            ->assertJson([
+                'success' => true,
+                'message' => 'User logged in successfully!',
+            ]);
+    }
+
+    public function test_invalid_user_login()
+    {
+        $credentials = [
+            'email' => 'emailthatnotexists@example.com',
+            'password' => 'examplepassword123',
+        ];
+
+        $this->postJson('/api/users/login', $credentials)
+            ->assertStatus(401)
+            ->assertJson([
+                'success' => false,
+                'message' => 'Incorrect login or password.',
+            ]);
+    }
+}
