1cc60e1e1db03b40ca460f3c2afbbe2dc45d20fe
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2024-06-20 15:52:54 -0300

    test: add user registration test

---
+++ tests/Feature/UserRegistrationTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserRegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_register()
+    {
+        $userData = [
+            'name' => 'Example name',
+            'email' => 'email@example.com',
+            'password' => 'examplepassword123'
+        ];
+
+        $response = $this->postJson('/api/users', $userData);
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'email@example.com'
+        ]);
+
+        $response->assertStatus(201)
+            ->assertJson([
+                'success' => true,
+                'message' => 'User created successfully!',
+                'data' => [
+                    'user' => []
+                ]
+            ]);
+    }
+
+    public function test_duplicate_email_validation_error_when_registering_user()
+    {
+        $user1 = [
+            'name' => 'User 1',
+            'email' => 'user@example.com',
+            'password' => 'examplepassword123'
+        ];
+
+        $this->postJson('/api/users', $user1);
+
+        $user2 = [
+            'name' => 'User 2',
+            'email' => 'user@example.com',
+            'password' => 'examplepassword123'
+        ];
+
+        $response = $this->postJson('/api/users', $user2);
+
+        $response->assertStatus(409)
+            ->assertJson([
+                'success' => false,
+                'data' => null,
+                'message' => 'E-mail is already in use'
+            ]);
+    }
+}
