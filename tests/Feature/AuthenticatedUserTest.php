41a87b3408497a95efa9109739f8f6f16caffc44
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2024-06-21 11:43:22 -0300

    test: add tests for authenticated user

---
+++ tests/Feature/AuthenticatedUserTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthenticatedUserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_authenticated_user_data_retrieval()
+    {
+        $user = User::factory()->create([
+            'name' => 'Example name',
+            'email' => 'email@example.com',
+            'password' => 'examplepassword123'
+        ]);
+    
+        $credentials = [
+            'email' => 'email@example.com',
+            'password' => 'examplepassword123',
+        ];
+
+        $response = $this->postJson('/api/users/login', $credentials);
+
+        $token = $response->json('data.access_token');
+
+        $meResponse = $this->withHeaders([
+            'Authorization' => "Bearer $token",
+        ])->getJson('/api/users/me');
+
+        $meResponse->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+                'message' => 'User data retrieved successfully',
+            ]);
+    }
+
+    public function test_unauthenticated_user_data_retrieval()
+    {
+        $response = $this->getJson('/api/users/me');
+
+        $response->assertStatus(401)
+            ->assertJson([
+                'message' => 'Unauthenticated.',
+            ]);
+    }
+}
